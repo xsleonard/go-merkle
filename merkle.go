c3e38f528ebb0b87d1942db0f9d82e50c2965bf8
Steve Leonard <xsleonard@gmail.com>
2014-02-13 21:26:55 -0500

    fix docstrings

--- merkle.go
+++ merkle.go
@@ -3,6 +3,56 @@ Use of this source code is governed by the MIT license that can be found
 in the LICENSE file.
 */
 
+/* A fixed merkle tree implementation
+
+Example use:
+
+    package main
+
+    import (
+        "crypto/md5"
+        "fmt"
+        "github.com/xsleonard/merkle"
+        "io/ioutil"
+    )
+
+    func splitData(data []byte, size int) [][]byte {
+        // Splits data into an array of slices of len(size)
+        count := len(data) / size
+        blocks := make([][]byte, 0, count)
+        for i := 0; i < count; i++ {
+            block := data[i*size : (i+1)*size]
+            blocks = append(blocks, block)
+        }
+        if len(data)%size != 0 {
+            blocks = append(blocks, data[len(blocks)*size:])
+        }
+        return blocks
+    }
+
+    func main() {
+        // Grab some data to make the tree out of, and partition
+        data, err := ioutil.ReadFile("testdata") // assume testdata exists
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        blocks := splitData(data, 32)
+
+        // Create & generate the tree
+        tree := merkle.NewTree()
+        err = tree.Generate(blocks, md5.New())
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+
+        fmt.Printf("Height: %d\n", tree.Height())
+        fmt.Printf("Root: %v\n", tree.Root())
+        fmt.Printf("N Leaves: %v\n", len(tree.Leaves()))
+        fmt.Printf("Height 2: %v\n", tree.GetNodesAtHeight(2))
+    }
+*/
 package merkle
 
 import (
@@ -10,12 +60,14 @@ import (
     "hash"
 )
 
+// A node in the merkle tree
 type Node struct {
     Hash  []byte
     Left  *Node
     Right *Node
 }
 
+// Creates a node given a hash function and data to hash
 func NewNode(h hash.Hash, block []byte) (Node, error) {
     if h == nil || block == nil {
         return Node{}, nil
@@ -28,8 +80,11 @@ func NewNode(h hash.Hash, block []byte) (Node, error) {
     return Node{Hash: h.Sum(nil)}, nil
 }
 
+// Contains all nodes
 type Tree struct {
-    Nodes  []Node
+    // All nodes, linear
+    Nodes []Node
+    // Points to each level in the node. The first level contains the root node
     Levels [][]Node
 }
 
@@ -37,8 +92,8 @@ func NewTree() Tree {
     return Tree{Nodes: nil, Levels: nil}
 }
 
+// Returns a slice of the leaf nodes in the tree, if available, else nil
 func (self *Tree) Leaves() []Node {
-    /* Returns a slice of the leaf nodes in the tree, if available, else nil */
     if self.Levels == nil {
         return nil
     } else {
@@ -46,8 +101,8 @@ func (self *Tree) Leaves() []Node {
     }
 }
 
+// Returns the root node of the tree, if available, else nil
 func (self *Tree) Root() *Node {
-    /* Returns the root node of the tree, if available, else nil */
     if self.Nodes == nil {
         return nil
     } else {
@@ -55,10 +110,10 @@ func (self *Tree) Root() *Node {
     }
 }
 
+// Returns all nodes at a given height, where height 1 returns a 1-element
+// slice containing the root node, and a height of tree.Height() returns
+// the leaves
 func (self *Tree) GetNodesAtHeight(h uint64) []Node {
-    /* Returns all nodes at a given height, where height 1 returns a 1-element
-       slice containing the root node, and a height of tree.Height() returns
-       the leaves */
     if self.Levels == nil || h == 0 || h > uint64(len(self.Levels)) {
         return nil
     } else {
@@ -66,8 +121,8 @@ func (self *Tree) GetNodesAtHeight(h uint64) []Node {
     }
 }
 
+// Returns the height of this tree
 func (self *Tree) Height() uint64 {
-    /* Returns the height of this tree */
     if self.Levels == nil {
         return 0
     } else {
@@ -75,8 +130,8 @@ func (self *Tree) Height() uint64 {
     }
 }
 
+// Generates the tree nodes
 func (self *Tree) Generate(blocks [][]byte, hashf hash.Hash) error {
-    /* Generates the tree nodes */
     if blocks == nil {
         return errors.New("Blocks must be non-nil")
     }
@@ -124,17 +179,17 @@ func (self *Tree) Generate(blocks [][]byte, hashf hash.Hash) error {
     return nil
 }
 
+// Creates all the non-leaf nodes for a certain height. The number of nodes
+// is calculated to be 1/2 the number of nodes in the lower rung.  The newly
+// created nodes will reference their Left and Right children
 func (self *Tree) generateNodeLevel(below []Node, current []Node, h hash.Hash) ([]Node, error) {
-    /* Creates all the non-leaf nodes for a certain height. The number of nodes
-       is calculated to be 1/2 the number of nodes in the lower rung.  The newly
-       created nodes will reference their Left and Right children */
     size := h.Size()
     data := make([]byte, size*2)
     end := (len(below) + (len(below) % 2)) / 2
     for i := 0; i < end; i++ {
         // Concatenate the two children hashes and hash them, if both are
         // available, otherwise reuse the hash from the lone left node
-        var node Node
+        node := Node{}
         ileft := 2 * i
         iright := 2*i + 1
         left := &below[ileft]
@@ -166,17 +221,16 @@ func (self *Tree) generateNodeLevel(below []Node, current []Node, h hash.Hash) (
     return current, nil
 }
 
+// Calculates the number of nodes in a binary tree unbalanced strictly on
+// the right side.  Height is assumed to be equal to
+// CalculateTreeHeight(CalculateNodeCount(size))
 func CalculateUnbalancedNodeCount(height uint64, size uint64) uint64 {
-    /* Calculates the number of nodes in a binary tree unbalanced strictly on
-       the right side.  Height is assumed to be equal to
-       CalculateTreeHeight(CalculateNodeCount(size))
-    */
     if isPowerOfTwo(size) {
         return CalculateNodeCount(size)
     }
     count := size
     prev := size
-    var i uint64 = 1
+    i := uint64(1)
     for ; i < height; i++ {
         next := (prev + (prev % 2)) / 2
         count += next
@@ -185,9 +239,9 @@ func CalculateUnbalancedNodeCount(height uint64, size uint64) uint64 {
     return count
 }
 
+// Returns the number of nodes in a Merkle tree given the number
+// of elements in the data the tree is based on
 func CalculateNodeCount(element_count uint64) uint64 {
-    /* Returns the number of nodes in a Merkle tree given the number
-       of elements in the data the tree is based on */
     // Pad the count to the next highest multiple of 4
     if element_count == 0 {
         return 0
@@ -199,21 +253,20 @@ func CalculateNodeCount(element_count uint64) uint64 {
     return 2*element_count - 1
 }
 
+// Returns the height of a full, complete binary tree given node_count nodes
 func CalculateTreeHeight(node_count uint64) uint64 {
-    /* Returns the height of a full, complete binary tree given node_count
-       nodes */
     return ceilLogBaseTwo(node_count)
 }
 
+// Returns true if n is a power of 2
 func isPowerOfTwo(n uint64) bool {
-    /* Returns true if n is a power of 2 */
     // http://graphics.stanford.edu/~seander/bithacks.html#DetermineIfPowerOf2
     return n != 0 && (n&(n-1)) == 0
 }
 
+// Returns the next highest power of 2 above n, if n is not already a
+// power of 2
 func nextPowerOfTwo(n uint64) uint64 {
-    /* Returns the next highest power of 2 above n, if n is not already a
-       power of 2 */
     if n == 0 {
         return 1
     }
@@ -229,8 +282,8 @@ func nextPowerOfTwo(n uint64) uint64 {
     return n
 }
 
+// Lookup table for integer log2 implementation
 var log2lookup []uint64 = []uint64{
-    /* Lookup table for integer log2 implementation */
     0xFFFFFFFF00000000,
     0x00000000FFFF0000,
     0x000000000000FF00,
@@ -239,15 +292,15 @@ var log2lookup []uint64 = []uint64{
     0x0000000000000002,
 }
 
+// Returns the log2 value of n
+// See: http://stackoverflow.com/a/15327567
 func ceilLogBaseTwo(x uint64) uint64 {
-    /* Returns the log2 value of n
-       http://stackoverflow.com/a/15327567 */
-    var y uint64 = 1
+    y := uint64(1)
     if (x & (x - 1)) == 0 {
         y = 0
     }
-    var j uint64 = 32
-    var i uint64 = 0
+    j := uint64(32)
+    i := uint64(0)
 
     for ; i < 6; i++ {
         k := j
